c8b9d1aadfa185081c2cabf822e306a7742034e4
Mustafa <yusufmustufa@gmail.com>
2024-06-16 15:56:46 +0530

    Setting up router-dom and making the needed componenets

--- cart-app/src/App.jsx
+++ cart-app/src/App.jsx
@@ -1,8 +1,22 @@
 /* import React from 'react' */
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element = { <Home/> }  />
+          <Route path="/cart" element = { <Cart/> }  />
+          
+        </Routes>
+      </Router>
+    </>
+    
   )
 }
 
